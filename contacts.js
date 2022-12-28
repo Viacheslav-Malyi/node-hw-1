75d91cc430404384936de037d14552acb5bd04c4
Viacheslav-Malyi <Malyislava@gmail.com>
2022-12-28 17:31:57 +0100

    1

--- contacts.js
+++ contacts.js
@@ -8,7 +8,7 @@ async function listContacts() {
   try {
     const contacts = await fs.readFile(contactsPath);
     const conyactsParse = JSON.parse(contacts);
-    console.table(conyactsParse);
+
     return conyactsParse;
   } catch (error) {
     console.error(error);
@@ -18,8 +18,8 @@ async function listContacts() {
 async function getContactById(contactId) {
   try {
     const contacts = await listContacts();
-    const contactById = contacts.filter((el) => el.id === contactId);
-    console.table(contactById);
+    const contactById = contacts.find((el) => el.id === contactId);
+    return contactById;
   } catch (error) {
     console.error(error);
   }
@@ -31,7 +31,7 @@ async function removeContact(contactId) {
     const updateContcts = contacts.filter(
       (contact) => contact.id !== contactId
     );
-    console.table(updateContcts);
+
     await fs.writeFile(contactsPath, JSON.stringify(updateContcts, null, 2));
   } catch (error) {
     console.error(error);
@@ -44,7 +44,6 @@ async function addContact(name, email, phone) {
     const contact = { id, name, email, phone };
     const contacts = await listContacts();
     contacts.push(contact);
-    console.table(contacts);
     await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
   } catch (error) {
     console.error(error);
--- index.js
+++ index.js
@@ -11,21 +11,28 @@ async function invokeAction({ action, id, name, email, phone }) {
   switch (action) {
     case "list":
       await listContacts();
-
+      console.table(await listContacts());
       break;
 
     case "get":
+      console.log("invoke get");
       await getContactById(id);
+      console.table(await listContacts());
+      console.table(await getContactById(id));
       break;
 
     case "add":
       console.log("invoke add");
+      console.table(await listContacts());
       await addContact(name, email, phone);
+
+      console.table(await listContacts());
       break;
 
     case "remove":
+      console.table(await listContacts());
       await removeContact(id);
-      console.log("invoke remove");
+      console.table(await listContacts());
       break;
 
     default:
